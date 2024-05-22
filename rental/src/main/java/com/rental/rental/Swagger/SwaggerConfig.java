3d3e754dfe656523b81c403b4b9236f5db6a7605
9109898N <alioune.beye@sncf.fr>
2024-05-22 21:40:55 +0200

    Configuration autorisation du swagger avec token

--- rental/src/main/java/com/rental/rental/Swagger/SwaggerConfig.java
+++ rental/src/main/java/com/rental/rental/Swagger/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.rental.rental.Swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,27 +22,28 @@ public class SwaggerConfig {
      *
      * @return GroupedOpenApi configuré pour les chemins d'API publics.
      */
-        @Bean
-        public GroupedOpenApi publicApi() {
-            return GroupedOpenApi.builder()
-                    .group("public-apis")
-                    .pathsToMatch("/api/**","/images/**")
-                    .build();
-        }
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
 
     /**
      * Configure les métadonnées de l'API pour Swagger.
      *
      * @return OpenAPI configuré avec les informations de l'API.
      */
-        @Bean
-        public OpenAPI customOpenAPI() {
-            return new OpenAPI()
-                    .info(new Info()
-                            .title("Rental API")
-                            .version("1.0")
-                            .description("Voici l'api rental , creer dans le cadre d'un projet OCR par Beye alioune")
-                            .termsOfService("http://swagger.io/terms/")
-                            .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-        }
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("My REST API")
+                        .description("Some custom description of API.")
+                        .version("1.0").contact(new Contact().name("Sallo Szrajbman")
+                                .email( "www.baeldung.com").url("salloszraj@gmail.com"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
 }
