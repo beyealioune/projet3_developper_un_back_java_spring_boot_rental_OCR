b858beef6c605f98ed39bc6848c26126e9db15d5
9109898N <alioune.beye@sncf.fr>
2024-06-17 08:36:11 +0200

    Refacto du code

--- rental/src/main/java/com/rental/rental/services/AuthService.java
+++ rental/src/main/java/com/rental/rental/services/AuthService.java
@@ -41,19 +41,15 @@ public class AuthService {
 
         HashMap<String, String> response = new HashMap<>();
 
-        // vérifier en base de donnée le mail fourni par l'utilisateur
         Optional<User> myUser =  userRepository.findByEmail(userDTO.getEmail());
 
         // si l'utilisateur existe et que le mot de passe correspond
         if(myUser.isPresent() && bCryptPasswordEncoder.matches(userDTO.getPassword(), myUser.get().getPassword())) {
-            // Authentification de l'utilisateur et génération du token JWT
             String token = jwtUtil.generateToken(myUser.get());
-            // Ajout du token à la réponse
             response.put("token", token);
 
             System.out.println("Login successful for email: " + userDTO.getEmail());
         } else {
-            // Si l'utilisateur n'existe pas ou que le mot de passe est incorrect
             response.put("error", "Identifiants invalides");
             System.out.println("Login failed for email: " + userDTO.getEmail());
         }
